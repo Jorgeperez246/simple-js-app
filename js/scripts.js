3428a9a891ec9621fcb0c31b167c8142efc6e0d8
jorgeperez246 <jorgeperez246.jp@gmail.com>
2022-02-08 18:02:22 -0500

    added 4 objects to 'pokemonList'

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+let pokemonList = [
+  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+  { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
+  { name: "Fearow", height: 1.2, types: ["lying", "normal"] },
+  { name: "Weedle", height: 0.3, types: ["bug", "poison"] },
+];
