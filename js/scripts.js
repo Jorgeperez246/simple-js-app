37abc5b83d8c9e3de4673da3d785195cd9188380
Jorge Perez <jorgeperez246@softwaredev.mynetworksettings.com>
2022-02-10 04:23:37 -0500

    Added a loop that iterates over pokemonList and writes the name and height to the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
+// array of objects that have pokemon and their stats
 let pokemonList = [
   { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
   { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
   { name: "Fearow", height: 1.2, types: ["lying", "normal"] },
   { name: "Weedle", height: 0.3, types: ["bug", "poison"] },
 ];
+//loop to iterate over the objects
+for(let x = 0;x<pokemonList.length;x++){
+  if(pokemonList[x].height>1.0){
+  document.write(`${pokemonList[x].name} (height: ${pokemonList[x].height}) - Wow, that's big! <br>`);
+}
+ else {
+  document.write(`${pokemonList[x].name} (height: ${pokemonList[x].height}) <br>`);
+ }
+}
