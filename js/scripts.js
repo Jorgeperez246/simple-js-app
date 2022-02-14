c2b205793f009f143f7fa49c63e4a40ee04da278
jorgeperez246 <jorgeperez246.jp@gmail.com>
2022-02-14 16:03:06 -0500

    assigned new IIFE to pokemonRepository variable and updated my for loop to iterate over the pokemonRepository.getAll() function

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,35 @@
 // array of objects that have pokemon and their stats
-let pokemonList = [
-  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-  { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
-  { name: "Fearow", height: 1.2, types: ["lying", "normal"] },
-  { name: "Weedle", height: 0.3, types: ["bug", "poison"] },
-];
+
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
+    { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
+    { name: "Fearow", height: 1.2, types: ["lying", "normal"] },
+    { name: "Weedle", height: 0.3, types: ["bug", "poison"] },
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+  function add(item) {
+    pokemonList.push(item);
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
 //loop to iterate over the objects
 
-pokemonList.forEach(item => {
-  if(item.height > 1.0){
-    document.write(`${item.name} (height: ${item.height}) - Wow, that's big! <br>`);
-  }
-  else{
+pokemonRepository.getAll().forEach((item) => {
+  if (item.height > 1.0) {
+    document.write(
+      `${item.name} (height: ${item.height}) - Wow, that's big! <br>`
+    );
+  } else {
     document.write(`${item.name} (height: ${item.height}) <br>`);
   }
-})
- 
+});
 
 // testing git authorship with this comment
