aff6d70a34605aaedc207ef6d6d98ab7416659be
jorgeperez246 <jorgeperez246.jp@gmail.com>
2022-02-15 21:23:18 -0500

    added a button class to css file, added showdetails function w/ even handler in js file, and added function to add items to ul tag in the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -11,25 +11,44 @@ let pokemonRepository = (function () {
   function getAll() {
     return pokemonList;
   }
-  function add(item) {
-    pokemonList.push(item);
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
+  }
+  function addListItem(pokemon) {
+    let pokeList = document.querySelector(".pokemon-list");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    pokeList.appendChild(listItem);
+    listItem.appendChild(button);
+    button.classList.add("button-class");
+    button.addEventListener("click", showDetails);
+  }
+  // this will show the details of the pokemon.
+  function showDetails(pokemon) {
+    console.log(pokemon);
   }
 
   return {
     add: add,
     getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails,
   };
 })();
 //loop to iterate over the objects
 
-pokemonRepository.getAll().forEach((item) => {
-  if (item.height > 1.0) {
-    document.write(
-      `${item.name} (height: ${item.height}) - Wow, that's big! <br>`
-    );
-  } else {
-    document.write(`${item.name} (height: ${item.height}) <br>`);
-  }
+pokemonRepository.getAll().forEach((pokemon) => {
+  pokemonRepository.addListItem(pokemon);
 });
 
 // testing git authorship with this comment
