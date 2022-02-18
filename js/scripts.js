2a332fc4281bea5927fad59d228209674183ca0f
jorgeperez246 <jorgeperez246.jp@gmail.com>
2022-02-18 13:15:06 -0500

    Added loadList, loadDetails, and changed addListItem to load list from API. showDetails now logs image url height and type into the console from api

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,13 @@
 // array of objects that have pokemon and their stats
 
 let pokemonRepository = (function () {
-  let pokemonList = [
-    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-    { name: "Charizard", height: 1.7, types: ["fire", "flying"] },
-    { name: "Fearow", height: 1.2, types: ["lying", "normal"] },
-    { name: "Weedle", height: 0.3, types: ["bug", "poison"] },
-  ];
-
+  let pokemonList = [];
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
   function getAll() {
     return pokemonList;
   }
   function add(pokemon) {
-    if (
-      typeof pokemon === "object" &&
-      "name" in pokemon &&
-      "height" in pokemon &&
-      "types" in pokemon
-    ) {
+    if (typeof pokemon === "object" && "detailsUrl" in pokemon) {
       pokemonList.push(pokemon);
     } else {
       console.log("pokemon is not correct");
@@ -31,26 +21,63 @@ let pokemonRepository = (function () {
     pokeList.appendChild(listItem);
     listItem.appendChild(button);
     button.classList.add("button-class");
-    button.addEventListener("click", function () {
+    button.addEventListener("click", (event) => {
       showDetails(pokemon);
     });
   }
   // this will show the details of the pokemon.
   function showDetails(pokemon) {
-    console.log(pokemon);
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
   }
-
+  function loadList() {
+    return fetch(apiUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        json.results.forEach((item) => {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon);
+        });
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((details) => {
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
     showDetails: showDetails,
+    loadList: loadList,
+    loadDetails: loadDetails,
   };
 })();
 //loop to iterate over the objects
-
-pokemonRepository.getAll().forEach((pokemon) => {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach((pokemon) => {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
 
 // testing git authorship with this comment
